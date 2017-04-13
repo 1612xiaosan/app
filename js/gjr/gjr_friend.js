481bf74ba4437f2032dd66cb133bfea4be46a057
FruitGan <1362597276@qq.com>
2017-04-13 23:28:46 +0800

    share

---
+++ js/gjr/gjr_friend.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	$('.gjr_shead').children().each(function(){
+		$(this).click(function(){
+			$('.gjr_shead').find('span').removeClass('gjr_first');
+			$(this).addClass('gjr_one').siblings($(this)).removeClass("gjr_one");
+			$(this).find('span').addClass('gjr_first');
+		})
+	})
+	$('.gjr_share>img:first').click(function(){
+		$('body,html').toggleClass('hidden');
+		$('.gjr_alert').slideToggle()
+	})
+		
+	
+})
