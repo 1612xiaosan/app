3ca14b989485d3b22edb64cd5e5fd58a51ea7e6d
yangzefeng520 <1213128156@qq.com>
2017-04-13 17:43:16 +0800

    sd

--- js/yzf-js/sousuo.js
+++ js/yzf-js/sousuo.js
@@ -1,10 +1,22 @@
 $(document).ready(function(){
-    var swiper = new Swiper('.yzf-swiper-container',{
-        paginationClickable: true,
-        spaceBetween: 60,
-        centeredSlides: true,
-        autoplay: 2500,
-        autoplayDisableOnInteraction: false
+	 var appendNumber = 5;
+    var prependNumber = 1;
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        slidesPerView: 3,
+        centeredSlides: false,
+        paginationClickable: false,
+        spaceBetween: 30,
     })
-//		console.log(swiper)
+    
+    
+    
+//  获取焦点
+	$('.yzf-form input').focus(function(value){
+//		console.log(1)
+		$(this).attr('value','');
+		location.href('../../html/yzf-html/sousuo2.html');
+	})
 })
