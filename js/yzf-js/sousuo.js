024c09349fa76cbce5dd01fdbe7b531ac11b484e
yangzefeng520 <1213128156@qq.com>
2017-04-13 14:36:33 +0800

    ss

---
+++ js/yzf-js/sousuo.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    var swiper = new Swiper('.yzf-swiper-container',{
+        paginationClickable: true,
+        spaceBetween: 60,
+        centeredSlides: true,
+        autoplay: 2500,
+        autoplayDisableOnInteraction: false
+    })
+//		console.log(swiper)
+})
