c39821791f53d310b7319bc416bf9481d53785c0
unknown <>
2017-04-13 17:03:32 +0800

    common

---
+++ js/hsf/hsf2.js
@@ -0,0 +1,9 @@
+$(function(){
+	$('input').focus(function(){
+		$('.hsf-two-youpin').css("display","none");
+		$('.hsf-two-xinxi').css("margin-top","0.3rem");
+		$('.hsf-two-continue').css("display","none");
+		$('.hsf-two-continue').css("display","none");
+		$('.hsf-two-next').css("display","block");
+	})
+})
