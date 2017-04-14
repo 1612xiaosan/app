bfd1e27d11c74a2a0d59379ebdeda7b27fa23f57
unknown <>
2017-04-14 15:47:45 +0800

    注册登录跳转测试

--- js/hsf-zhuce.js
+++ js/hsf-zhuce.js
@@ -11,8 +11,19 @@ $(function(){
 		$('.hsf-two-biaodan').css("display","none");
 		$('.hsf-two-images').css("display","block");
 		$('.hsf-two-next').css("display","none");
-		$('.hsf-two-continue').css("display","block");	
+		$('.hsf-two-continues').css("display","block");	
+	})
+	$('.hsf-two-continues').on("click",function(){
+         $('.hsf-two-bianhuan').css("display","none");
+		$('.hsf-two-bianhuan2').css("display","block");
+	})
+	$('.zxq-wczc').on("click",function(){
+		location.assign("hsf-denglu.html")
+	})
+		$('.hsf-two-continue').on("click",function(){
+		alert("请填写内容！")
 	})
 	
-
+	
+	
 })
