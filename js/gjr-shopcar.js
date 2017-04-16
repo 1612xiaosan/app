b7ed3a557140518e2ec5e3e5a1ac4d2b0d2c0171
yangzefeng520 <1213128156@qq.com>
2017-04-17 00:44:46 +0800

    zhifu

--- js/gjr-shopcar.js
+++ js/gjr-shopcar.js
@@ -34,5 +34,21 @@ $(document).ready(function(){
 			$('.gjr_img2 img').removeClass('yzf-active2').eq($(this).index()-1).addClass('yzf-active2')
 		})
 	})
+	$('.gjr_color3 > span:first-child ~ span').each(function(index,value){
+		console.log($(this))
+		$(this).on('touchstart',function(){
+//			console.log($(this))
+			$(this).addClass('yzf-active').siblings().removeClass('yzf-active')
+			$('.gjr_img3 img').removeClass('yzf-active2').eq($(this).index()-1).addClass('yzf-active2')
+		})
+	})
+	$('.gjr_color22 > span:first-child ~ span').each(function(index,value){
+		console.log($(this))
+		$(this).on('touchstart',function(){
+//			console.log($(this))
+			$(this).addClass('yzf-active3').siblings().removeClass('yzf-active3')
+			$('.gjr_img3 img').removeClass('yzf-active2').eq($(this).index()-1).addClass('yzf-active2')
+		})
+	})
 	
 })
