382b5bb04732415e76cfd728822f3606d5fbb842
FruitGan <1362597276@qq.com>
2017-04-16 13:32:37 +0800

    js

--- js/gjr_goods.js
+++ js/gjr_goods.js
@@ -14,9 +14,15 @@ $(document).ready(function(){
 ////		console.log(e.offsetX)
 //	})
 	let tiao=document.querySelector('.gjr_big');
+	let c2=document.querySelector('.circle2');
+	let c1=document.querySelector('.circle1');
+	
 //	console.log(tiao)
 	tiao.ontouchstart=function(e){
-		alert(1)
+		let left=e.touches[0].pageX;
+		c2.style.left=left+'px';
+		let left1=e.touches[1].pageX;
+		c1.style.left=left1+'px';
 	}
 //	tiao.ontouchstart=function(e){
 //		let left=e.touches[0].pageX;
