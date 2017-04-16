f403fd022f824ff5002f5249102f12a7fc6fbc71
unknown <>
2017-04-16 15:01:09 +0800

    选择银行卡实验

---
+++ js/hsf-querendingdan.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$('.hsf-qrdd-yinghangka').on("click",function(){
+		$('.hsf-qrdd-baowei').css("display","none");
+		$('.hsf-qrdd-querenzhifu').css("display","block");
+	})
+	$('.hsf-qrdd-querenanniu').on("click",function(){
+		$('.hsf-qrdd-querenzhifu').css("display","none");
+		$('.hsf-qrdd-xuanzeihangka').css("display","block");
+	})
+	
+	
+	
+})
+
