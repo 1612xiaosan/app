23c653706d1c8efc45fb2c1ad0c41d7699852cc8
unknown <>
2017-04-17 17:31:21 +0800

    这是一次重要的修改

--- js/hsf-querendingdan.js
+++ js/hsf-querendingdan.js
@@ -1,21 +1,28 @@
 $(document).ready(function(){
+		$('.hsf-qrdd-success').css("display","none");					
+	
 	$('.hsf-qrdd-yinghangka').on("click",function(){
 		$('.hsf-qrdd-baowei').css("display","none");
+		$('.hsf-qrdd-success').css("display","none");					
 		$('.hsf-qrdd-querenzhifu').css("display","block");
 	})
 	$('.hsf-qrdd-querenanniu').on("click",function(){
 		$('.hsf-qrdd-querenzhifu').css("display","none");
+		$('.hsf-qrdd-success').css("display","none");					
 		$('.hsf-qrdd-xuanzeihangka').css("display","block");
 	})
 	$('.hsf-qrdd-tianjia').on("click",function(){
+		$('.hsf-qrdd-success').css("display","none");					
 		$('.hsf-qrdd-xuanzeihangka').css("display","none");
 		$('.hsf-qrdd-tianjiayinhangka').css("display","block");
 	})
 	$('.hsf-qrdd-circles').on("click",function(){
 		$('.hsf-qrdd-xuanzeihangka').css("display","none");
+		$('.hsf-qrdd-success').css("display","none");					
 		$('.hsf-qrdd-baowei').css("display","block");
 	})
 	$('.hsf-bottom').on("click",function(){
+		$('.hsf-qrdd-success').css("display","none");			
 		$('.hsf-qrdd-tianjiayinhangka').css("display","none");
 		$('.hsf-qrdd-threeshibie').css("display","block");		
 		$('.hsf-qrdd-zhiwenshibie').css("display","block");
@@ -25,27 +32,20 @@ $(document).ready(function(){
 		$('.hsf-qrdd-baowei').css("display","none");		
 		$('.hsf-qrdd-tianjiayinhangka').css("display","none");
 		$('.hsf-qrdd-tianjiayinhangka').css("display","none");
-		$('.hsf-qrdd-threeshibie').css("display","block");
-		$('.hsf-qrdd-zhiwenshibie').css("display","block");
+		$('.hsf-qrdd-success').css("display","none");
+		$('.tu').css("display","block");
 	})
+	
 //	支付方式的转换
-var zhiwenshibie=document.getElementById("zhiwenshibie");
-var mimashibie=document.getElementById("mimashibie");
-var tuanshibie=document.getElementById("tuanshibie");
- zhiwenshibie.addEventListener("touchmove",function(){
- 	zhiwenshibie.style.display="none";
- 	mimashibie.style.display="block";
- })
- mimashibie.addEventListener("touchmove",function(){
- 	mimashibie.style.display="none";
- 	tuanshibie.style.display="block";
- })
-  tuanshibie.addEventListener("touchmove",function(){
- 	tuanshibie.style.display="none";
- 	zhiwenshibie.style.display="block";
- })
+$('.tu').on("click",function(){
+		$('.tu').css("display","none");					
+		$('.hsf-qrdd-success').css("display","block");
+	})
+
 $('.hsf-qrdd-guanbi').click(function(){
 	$('.hsf-qrdd-threeshibie').css("display","none");
+		$('.hsf-qrdd-success').css("display","none");
+		$('.tu').css("display","none");		
 		$('.hsf-qrdd-baowei').css("display","block");
 })
 $('.hsf-qrdd-threeshibie').click(function(){
@@ -53,36 +53,41 @@ $('.hsf-qrdd-threeshibie').click(function(){
 		$('.hsf-qrdd-success').css("display","block");
 })
 $('.hsf-success-next').click(function(){
+	$('.hsf-qrdd-success').css("display","none");				
 	$('.hsf-success-look').removeClass("actives");
 	$('.hsf-success-next').addClass("actives");
 })
 $('.hsf-success-look').click(function(){
+	$('.hsf-qrdd-success').css("display","none");				
 	$('.hsf-success-next').removeClass("actives");
 	$('.hsf-success-look').addClass("actives");
 })
 $('.hsf-success-nexts').click(function(){
+	$('.hsf-qrdd-success').css("display","none");				
 	$('.hsf-success-looks').removeClass("actives");
 	$('.hsf-success-nexts').addClass("actives");
 })
 $('.hsf-success-looks').click(function(){
+		$('.hsf-qrdd-success').css("display","none");			
+	
 	$('.hsf-success-nexts').removeClass("actives");
 	$('.hsf-success-looks').addClass("actives");
 })
 	$('.hsf-success-nexts').on("click",function(){
 		$('.hsf-qrdd-querenzhifu').css("display","none");
+		$('.hsf-qrdd-success').css("display","none");				
 		$('.hsf-qrdd-xuanzeihangka').css("display","block");
 	})
 		$('.hsf-success-looks').on("click",function(){
+		$('.hsf-qrdd-success').css("display","none");		
 		$('.hsf-qrdd-querenzhifu').css("display","none");
 		$('.hsf-qrdd-baowei').css("display","block");
 	})
 		$('.fanhui').on("click",function(){
 		$('.hsf-qrdd-tianjiayinhangka').css("display","none");
+		$('.hsf-qrdd-success').css("display","none");		
 		$('.hsf-qrdd-xuanzeihangka').css("display","block");
 	})
-
-
-
 
 
 })
