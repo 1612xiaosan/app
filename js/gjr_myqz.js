f10fb4708a87d725a3e5e8a954883658ab04e43b
FruitGan <1362597276@qq.com>
2017-04-17 10:18:26 +0800

    voice

--- js/gjr_myqz.js
+++ js/gjr_myqz.js
@@ -1,12 +1,4 @@
-$(document).ready(function(){
-//	console.log($('.yzf-form'))
-	$('.yzf-form input').focus(function(value){
-//		console.log(1)
-		$(this).attr('value','');
-	})
-	
-	//链接到经典页面
-	$('.yzf-list')
+$(function(){
 	$('.gjr_voice').on('touchstart',function(){
 		$('body,html').addClass('hidden')
 		$('.gjr_alert').show();
